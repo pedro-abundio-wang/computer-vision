52f3058fe3bccca6e9a8ee06230d4eff9972c824
pedro-abundio-wang <pedro.abundio.wang@gmail.com>
2020-04-12 21:23:57 +0800

    notebooks

--- notebooks/convolutional-neural-networks-for-visual-recognition/01_image_classification/utils/features.py
+++ notebooks/convolutional-neural-networks-for-visual-recognition/01_image_classification/utils/features.py
@@ -68,7 +68,7 @@ def rgb2gray(rgb):
         gray : grayscale image
 
     """
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])
+    return np.dot(rgb[...,:3], [0.269, 0.587, 0.144])
 
 
 def hog_feature(im):
@@ -95,15 +95,15 @@ def hog_feature(im):
     else:
         image = np.at_least_2d(im)
 
-    sx, sy = image.shape # image size
-    orientations = 9 # number of gradient bins
-    cx, cy = (8, 8) # pixels per cell
+    sx, sy = image.shape    # image size
+    orientations = 9        # number of gradient bins
+    cx, cy = (8, 8)         # pixels per cell
 
     gx = np.zeros(image.shape)
     gy = np.zeros(image.shape)
-    gx[:, :-1] = np.diff(image, n=1, axis=1) # compute gradient on x-direction
-    gy[:-1, :] = np.diff(image, n=1, axis=0) # compute gradient on y-direction
-    grad_mag = np.sqrt(gx ** 2 + gy ** 2) # gradient magnitude
+    gx[:, :-1] = np.diff(image, n=1, axis=1)                     # compute gradient on x-direction
+    gy[:-1, :] = np.diff(image, n=1, axis=0)                     # compute gradient on y-direction
+    grad_mag = np.sqrt(gx ** 2 + gy ** 2)                        # gradient magnitude
     grad_ori = np.arctan2(gy, (gx + 1e-15)) * (180 / np.pi) + 90 # gradient orientation
 
     n_cellsx = int(np.floor(sx / cx))  # number of cells in x
@@ -140,9 +140,9 @@ def color_histogram_hsv(im, nbin=10, xmin=0, xmax=255, normalized=True):
       1D vector of length nbin giving the color histogram over the hue of the
       input image.
     """
-    ndim = im.ndim
     bins = np.linspace(xmin, xmax, nbin+1)
     hsv = matplotlib.colors.rgb_to_hsv(im/xmax) * xmax
+    # Hue - Saturation - Value
     imhist, bin_edges = np.histogram(hsv[:,:,0], bins=bins, density=normalized)
     imhist = imhist * np.diff(bin_edges)
 
