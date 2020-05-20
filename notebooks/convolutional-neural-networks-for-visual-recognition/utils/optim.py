2231160745489322244db11e0fb1f64d99e47433
pedro-abundio-wang <pedro.abundio.wang@gmail.com>
2020-05-20 17:00:22 +0800

    notebooks

--- notebooks/convolutional-neural-networks-for-visual-recognition/utils/optim.py
+++ notebooks/convolutional-neural-networks-for-visual-recognition/utils/optim.py
@@ -40,7 +40,14 @@ def sgd(w, dw, config=None):
     if config is None: config = {}
     config.setdefault('learning_rate', 1e-2)
 
-    w -= config['learning_rate'] * dw
+    # Track the ratio of weight updates / weight magnitudes
+    param_scale = np.linalg.norm(W.ravel())
+    update = -config['learning_rate'] * dw
+    update_scale = np.linalg.norm(update.ravel())
+    # want about 1e-3
+    update_scale / param_scale
+    
+    w += update
     return w, config
 
 
